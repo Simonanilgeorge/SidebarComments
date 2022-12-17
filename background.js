6ac4432be1cc86d3907e3152d0ef9d20b6f850ba
Simonanilgeorge <smnanilgeorge@gmail.com>
2022-12-17 20:14:46 +0530

    added comments

--- background.js
+++ background.js
@@ -1,16 +1,13 @@
-chrome.tabs.onUpdated.addListener((tabId, changInfo, tab) => {
-
-    if (changInfo && changInfo.status == "completed") {
-        try {
-
-            setTimeout(async() => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo && changeInfo.status == "complete") {
+        // wait for one second and send the message
+        setTimeout(async () => {
+            try {
                 let res = await chrome.tabs.sendMessage(tabId, { data: tab })
-            }, 1000)
-        }
-        catch (err) {
-            console.log(err)
-
-        }
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }, 1000)
     }
-})
-
+});
--- content.js
+++ content.js
@@ -1,44 +1,86 @@
+// set the initial message as false
+let messageSent = false;
+chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
 
-chrome.runtime.onMessage.addListener((req,sender,senderResponse)=>{
-  console.log("message received")
-  main()
-})
+  console.log("message called",messageSent)
+  // if page is loaded and message is received;this is used to prevent the calling of the main function more than once
+  if (!messageSent) {
+    console.log("message received");
+    // set the messageSent value as true to prevent multiple calls to main function
+    messageSent = true;
+    main();
+  }
+
+});
 
 
+// function to check if element exists in the page
 async function waitForElement(selector) {
+
+  // create a new promise;promise is resolved when element is found
   return new Promise((resolve, reject) => {
     const checkExists = () => {
+      console.log("check exist is called for element",selector)
       const element = document.querySelectorAll(selector)[0];
       if (element) {
         resolve(element);
       } else {
+        // repeatedly call the checkExists function to check if element is found;promise is resolved if element is found
         requestAnimationFrame(checkExists);
       }
     };
+
+    // call the function the first time
     checkExists();
   });
 }
 
+// main function
 async function main() {
 
+  // get the respective elements;
   let commentBox = await waitForElement("ytd-comments")
   let descriptionBox = await waitForElement("#bottom-row.ytd-watch-metadata")
   let videoRecommendations = await waitForElement("ytd-watch-flexy[flexy] #secondary.ytd-watch-flexy")
+
+  // set the width of the comment box the same as video recommendation box 
   commentBox.style.width = `${videoRecommendations.offsetWidth}px`
+
+  // add styles to comment box
   commentBox.classList.add("commentBox")
 
+  // add styles to video recommendation box
   videoRecommendations.classList.add("videoRecommendations")
 
-
+  // get the parent element of the video recommendation box
   let parent = videoRecommendations.parentNode
+  
+  // create a new parent element to hold comment box and recommendation box; the new parent element will be inside parent element for the video recommendation box
   let new_parent = document.createElement("div")
+
+  // add styles to the new parent element
   new_parent.classList.add("new-parent")
 
+  // add the new parent element to the parent element
   parent.appendChild(new_parent)
+  // append video recommendation box to the new parent element 
+  new_parent.appendChild(commentBox)
+  
+  //add video recommendation box to new parent element 
   new_parent.appendChild(videoRecommendations)
 
   // add comment box after videRecommendation
-  videoRecommendations.after(commentBox)
+  // commentBox.before(videoRecommendations)
+  // videoRecommendations.after(commentBox)
+
+}
 
 
+// custom function that can be used to wait for some time
+function wait(amount) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, amount)
+  })
 }
